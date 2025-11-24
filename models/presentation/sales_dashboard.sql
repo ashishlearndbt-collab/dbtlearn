c131f9634c0954120d16a0c41565e17ae25f95bd
ashish sABOO <ashishlearndbt@gmail.com>
2025-11-24 14:20:27 +0000

    COMMIT

--- models/presentation/sales_dashboard.sql
+++ models/presentation/sales_dashboard.sql
@@ -1,12 +1,34 @@
-{{ config(
-    materialized='incremental',
-    schema='presentation',
-    unique_key=['customer_id', 'order_day']
-) }}
+{% set from_date = var('from_date', none) %}
 
+{# ------------------------------ #}
+{#   PRE-HOOK: DELETE FOR RELOAD  #}
+{# ------------------------------ #}
+{% if from_date is not none %}
+  {{
+    config(
+      pre_hook = [
+       "delete from {{ this }} where reporting_date >= '{{ var('from_date') }}'"
+      ]
+       
+    )
+  }}
+{% endif %}
+
+{# ------------------------------ #}
+{#         MODEL CONFIG           #}
+{# ------------------------------ #}
+{{
+  config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key=['customer_id', 'order_day']
+  )
+}}
 
 with customer_summary as (
-    select DATEADD(day, 1, order_day) as reporting_date, *
+    select
+        dateadd(day, 1, order_day) as reporting_date,
+        *
     from {{ ref('int_sales_summary') }}
 ),
 
@@ -23,13 +45,28 @@ dashboard as (
     from customer_summary
 )
 
+{# ------------------------------ #}
+{#       INCREMENTAL LOGIC        #}
+{# ------------------------------ #}
+
 {% if is_incremental() %}
-    select *
-    from dashboard d
-    where d.order_day > (
-        select coalesce(max(order_day), '1900-01-01')
-        from {{ this }}
-    )
+
+    {% if from_date is not none %}
+        -- PARTIAL REFRESH MODE
+        -- after deletion, reinsert all rows for the range
+        select * from dashboard
+
+    {% else %}
+        -- NORMAL INCREMENTAL MODE
+        select *
+        from dashboard d
+        where d.order_day > (
+            select coalesce(max(order_day), '1900-01-01')
+            from {{ this }}
+        )
+    {% endif %}
+
 {% else %}
+    -- FULL REFRESH MODE
     select * from dashboard
 {% endif %}
